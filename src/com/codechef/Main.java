28b04d4698e43489390d61aab5b6ba5dc180cc1d
Nitin <nsinghboss02@gmail.com>
2022-01-03 20:49:13 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,21 @@
+package com.codechef;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// count no of vowels in an array:
+        Scanner sc = new Scanner(System.in);
+        int count =0;
+        char[] arr = new char[7];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.next().charAt(0);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=='a' || arr[i]=='e' || arr[i]=='i' || arr[i]=='o'||arr[i]=='u'||
+            arr[i]=='A'||arr[i]=='E'||arr[i]=='I'||arr[i]=='O'||arr[i]=='U'){
+                count++;
+            }
+        }
+        System.out.println("The number of vowels are "+count);
+    }
+}
