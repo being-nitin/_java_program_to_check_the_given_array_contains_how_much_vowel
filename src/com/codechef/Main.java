30f602e5bc0927675978e55172c2a5abe0b19169
Nitin <nsinghboss02@gmail.com>
2022-01-03 20:50:50 +0530

    both case characters added

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -10,6 +10,9 @@ public class Main {
         for(int i=0;i<arr.length;i++){
             arr[i] = sc.next().charAt(0);
         }
+        /*
+        it will contain both the uppercase and lowercase characters as a vowel:
+         */
         for(int i=0;i<arr.length;i++){
             if(arr[i]=='a' || arr[i]=='e' || arr[i]=='i' || arr[i]=='o'||arr[i]=='u'||
             arr[i]=='A'||arr[i]=='E'||arr[i]=='I'||arr[i]=='O'||arr[i]=='U'){
